168b16c4edf3717b43c93fc455975e2e1cc85872
otaiga <karl.turner5@gmail.com>
2012-08-22 15:31:59 +0100

    updated to have config variables

--- lib/jetsetmeapi.rb
+++ lib/jetsetmeapi.rb
@@ -1,4 +1,5 @@
 require "jetsetmeapi/version"
+require "jetsetmeapi/configuration"
 require 'httparty'
 require 'json'
 
@@ -6,13 +7,10 @@ module Jetsetmeapi
 
   AUTHPATH="https://api.jetsetme.com/authentication/request_token"
   CONFIRMPATH="https://api.jetsetme.com/authentication/confirm_token"
-  DEVID=ENV["DEVID"]
-  APPID=ENV["APPID"]
-  HEADERS = {"X-DEVID" => DEVID, "X-APPID" => APPID, "Content-type" => "application/json"}
 
   def self.get_auth_token(msisdn)
     if load_parameters
-      response = HTTParty.post(AUTHPATH, :headers => HEADERS, :body => {:msisdn => msisdn}.to_json)
+      response = HTTParty.post(AUTHPATH, :headers => @HEADERS, :body => {:msisdn => msisdn}.to_json)
       token = JSON.parse(response)["token"]
       error = JSON.parse(response)["error"] unless nil
       unless token == nil
@@ -21,23 +19,27 @@ module Jetsetmeapi
         return {:error => error}
       end
     else
-      return {:error => "ERROR not all ENV variables found"}
+      return {:error => "ERROR not all variables found"}
     end
   end
 
   def self.get_auth_key(pin, token)
     if load_parameters
-      response = HTTParty.post(CONFIRMPATH, :headers => HEADERS, :body => {:pin => pin, :token => token}.to_json)
+      response = HTTParty.post(CONFIRMPATH, :headers => @HEADERS, :body => {:pin => pin, :token => token}.to_json)
       key = JSON.parse(response)["auth_key"]
     else
-      return {:error => "ERROR not all ENV variables found"}
+      return {:error => "ERROR not all variables found"}
     end
   end
 
 protected
 
   def self.load_parameters
-    if DEVID && APPID
+    @DEVID = Jetsetmeapi::Config.dev_id
+    @APPID = Jetsetmeapi::Config.app_id
+    @HEADERS = {"X-DEVID" => @DEVID, "X-APPID" => @APPID, "Content-type" => "application/json"}
+
+    if @DEVID && @APPID
       return true
     end
   end
--- spec/jetsetmeapi/jetsetmeapi_spec.rb
+++ spec/jetsetmeapi/jetsetmeapi_spec.rb
@@ -5,11 +5,11 @@ describe "Jetsetmeapi" do
   describe "Invalid ENV variables" do
     
     it 'should return error string for get_auth_token' do
-      Jetsetmeapi.get_auth_token(given_msisdn).should == {:error => "ERROR not all ENV variables found"}
+      Jetsetmeapi.get_auth_token(given_msisdn).should == {:error => "ERROR not all variables found"}
     end
 
     it 'should return error string for get_auth_key' do
-      Jetsetmeapi.get_auth_key(given_pin, given_token).should == {:error => "ERROR not all ENV variables found"}
+      Jetsetmeapi.get_auth_key(given_pin, given_token).should == {:error => "ERROR not all variables found"}
     end
   end
 end
